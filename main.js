18cf74d8063ebde3e1084f847b210d44b0faff50
REDUAN <123816734+red33-wb@users.noreply.github.com>
2023-04-28 19:23:34 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let bt = document.querySelector("#btn")
+
+
+
+
+bt.addEventListener("click",myfn)
+
+function myfn(){
+    let dis = document.querySelector("#div2").className='div'
+    document.querySelector("#txt").innerHTML='hello world'
+}
+
